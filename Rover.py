36c03478e99039fd4f2cea798ccf5b7af8b0194e
Thomas Henrique <tom.kzy@gmail.com>
2017-05-30 03:45:26 -0300

    Adding main file

---
+++ Rover.py
@@ -0,0 +1,57 @@
+'''
+Created on 28 de mai de 2017
+
+@author: tomkz
+'''
+
+
+
+pCardeais = "NLSO"   
+'''Variavel com as siglas das posicoes cardeais (Norte - N, Leste - L, Sul - S, Oeste - O)'''
+
+movimentos = [(0,1), (1,0), (0, -1), (-1, 0)]   
+"""Vetor com as posicoes para a movimentar o rover """
+
+
+def girarR(pCardeal): 
+    '''Calcula a direcao cardeal do rover a direita'''   
+    position =  pCardeais.index(pCardeal) + 1   
+    posCard = pCardeais[position % 4] 
+    return posCard 
+    
+          
+def girarL(pCardeal): 
+    '''Calcula a direcao cardeal do rover a esquerda'''
+    position =  pCardeais.index(pCardeal) - 1 
+    posCard = pCardeais[position % 4] 
+    return posCard   
+
+
+def mover(corXY, posC): 
+    '''Calcula a movimentacao do rover nas coordenadas xy'''   
+    corXY = (corXY[0] + movimentos[posC][0], corXY[1] + movimentos[posC][1])
+    return corXY
+    
+
+def exeComandos(coordenadas, pCardeal, cmds):  
+    '''Executa os comandos do rover'''  
+    
+    for c in cmds:    
+        if (c == 'R'):
+            pCardeal = girarR(pCardeal) 
+             
+             
+        elif (c == 'L'):
+            pCardeal = girarL(pCardeal)
+            
+            
+        elif (c == 'M'):
+            coordenadas = mover(coordenadas, pCardeais.index(pCardeal))
+            
+    print(coordenadas, pCardeal)
+    
+    
+exeComandos((1,2), 'N',"LMLMLMLMM")
+exeComandos((3, 3), 'L', "MMRMMRMRRM")
+exeComandos((3, 3), 'L', "MMRMMRMRRMMMLLLR")
+        
